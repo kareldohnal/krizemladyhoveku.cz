d346e56ee63502b138808399f382cb8f769749bf
Karel Dohnal <karel.dohnal@gmail.com>
2023-01-03 16:34:26 +0100

    Dynamic routing

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,12 @@ import Link from 'next/link';
 const name = 'Karel Dohnal';
 export const siteTitle = 'Next.js Sample Website';
 
-export default function Layout({ children, home }) {
+type Props = {
+    children: JSX.Element[]
+    home?: boolean
+}
+
+export default function Layout({ children, home }: Props) {
     return (
         <div className={styles.container}>
             <Head>
